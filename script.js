257f183a408b538238d507a5499d9c0517cf893e
aaron96racc-cloud <aaron96racc@gmail.com>
2026-05-28 04:09:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+# script.js
+
+```javascript id="1bf5nk"
 const toggleButton = document.getElementById("theme-toggle");
 
 const body = document.body;
 
 const logo = document.getElementById("logo");
 
-toggleButton.addEventListener("click", () => {
+/* =========================
+   CARGAR TEMA GUARDADO
+========================= */
 
-body.classList.toggle("light-mode");
+const savedTheme = localStorage.getItem("theme");
 
-if(body.classList.contains("light-mode")){
+if(savedTheme === "light"){
+
+body.classList.add("light-mode");
 
 logo.src = "img/logo-light.png";
 
@@ -16,10 +23,49 @@ toggleButton.innerHTML = "☀️";
 
 }else{
 
+body.classList.remove("light-mode");
+
 logo.src = "img/logo-dark.png";
 
 toggleButton.innerHTML = "🌙";
 
 }
 
+/* =========================
+   CAMBIAR TEMA
+========================= */
+
+toggleButton.addEventListener("click", () => {
+
+body.classList.toggle("light-mode");
+
+/* ===== LIGHT MODE ===== */
+
+if(body.classList.contains("light-mode")){
+
+logo.src = "img/logo-light.png";
+
+toggleButton.innerHTML = "☀️";
+
+/* GUARDAR */
+
+localStorage.setItem("theme", "light");
+
+}
+
+/* ===== DARK MODE ===== */
+
+else{
+
+logo.src = "img/logo-dark.png";
+
+toggleButton.innerHTML = "🌙";
+
+/* GUARDAR */
+
+localStorage.setItem("theme", "dark");
+
+}
+
 });
+```
